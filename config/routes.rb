2dcaa13fb31d4bce8da0b1de41ffd93581da5ded
Jacob Bijani <jacob.bijani@gmail.com>
2014-04-21 14:01:56 -0400

    kill registrations, seed initial user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  devise_for :users, :skip => [:registrations]
 
-  # You can have the root of your site routed with "root"
-  root :to => "home#index"
+  root "home#index"
 
 end
